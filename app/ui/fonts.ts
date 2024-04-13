580ec90c20c916e7c5d1b2297014a0c69afcd5b4
js-easy-school <supergeroy11@gmail.com>
2024-04-13 10:57:30 +0300

    update

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
-import { Inter, Lusitana } from '@next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
 
 export const inter = Inter({subsets: ['latin']});
 export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
